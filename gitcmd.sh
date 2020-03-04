d769a3da8c1ce7be1f2230d8deb4ce13bbfde13c
53554 <53554@etu.he2b.be>
2020-03-04 20:13:28 +0100

    update

--- gitcmd.sh
+++ gitcmd.sh
@@ -2,26 +2,25 @@
 echo -n "Nouveau dépôt Y/N: "
 read ans
 if [ $ans== "y" ]
+then
 git init
-then git add .
+ git add .
 echo -n "git init && git add réussi"
 git commit -a 
 echo "Commit successfull"
-fi
-if [ $ans== "n" ]
+else [ $ans== "n" ]
 git add . && git commit -a
-then
 echo -n "git add et commit réussi"
 fi
 echo -n "Un pull est-il nécessaire Y/N: "
 read ans2
 if [ $ans2== "y" ]
-echo -n "Lien de votre dépôt git:"
 then
+echo -n "Lien de votre dépôt git:"
 read path1
 git pull $path1 
-fi
-if [ $ans2=="n" ]
+else [ $ans2=="n" ]
 echo -n "Lien git vers votre dépôt pour le push:"
 read path 
 git push $path
+echo -n "Pull successfull"
