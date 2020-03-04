3bfd674db11fd06bc7d7c6b5bb1898a2e9945d93
53554 <53554@etu.he2b.be>
2020-03-04 20:00:29 +0100

    gitCmd update

--- gitcmd.sh
+++ gitcmd.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-if git add .
-then 
-git commit -a
+echo -n "Nouveau dépôt Y/N: "
+read ans
+if [ $ans== "y" ]
+git init
+then git add .
+echo -n "git init && git add réussi"
+git commit -a 
+echo "Commit successfull"
 fi
-echo -n "Veuillez entrez le lien git vers votre dépôt distant:"
+if [ $ans== "n" ]
+git add . && git commit -a
+echo -n "git add et commit réussi"
+fi
+echo -n "Un pull est-il nécessaire Y/N: "
+read ans2
+if [ $ans2== "y" ]
+echo -n "Lien de votre dépôt git:"
+read path1
+git pull $path1 
+fi
+echo -n "Veuillez entrez le lien git vers votre dépôt pour le push distant:"
 read path 
 git push $path
